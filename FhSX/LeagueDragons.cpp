ec9750092833df2feb81a701df015f5c97fb453d
☯ IMPRESIVΞ <60080728+ImpresiveDEV@users.noreply.github.com>
2024-02-08 13:00:23 +0100

    Add files via upload

--- FhSX/LeagueDragons.cpp
+++ FhSX/LeagueDragons.cpp
@@ -2,9 +2,17 @@
 #include <windows.h>
 #include <winver.h>
 #include <iostream>
+#include <filesystem> 
 #include <curl/curl.h>
 #include "json.hpp"
 #include "Config.h"
+
+namespace fs = std::filesystem;
+
+static size_t WriteData(void* ptr, size_t size, size_t nmemb, FILE* stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
 
 std::string LeagueDragons::GetProductVersion(const std::string& filePath) {
     DWORD handle = 0;
@@ -25,6 +33,60 @@ std::string LeagueDragons::GetProductVersion(const std::string& filePath) {
 
     return productVersion;
 }
+
+void LeagueDragons::DownloadFile(const std::string& url, const std::string& filePath) {
+
+    CURL* curl;
+    FILE* file = nullptr; 
+    CURLcode res;
+
+    curl = curl_easy_init();
+    if (curl) {
+
+        errno_t err = fopen_s(&file, filePath.c_str(), "wb"); 
+        if (err == 0) { 
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteData);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+            res = curl_easy_perform(curl);
+            fclose(file); 
+
+            if (res != CURLE_OK)
+                std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        }
+        else {
+            std::cerr << "Failed to open file: " << filePath << std::endl;
+        }
+        curl_easy_cleanup(curl);
+    }
+}
+
+
+void LeagueDragons::EnsureDirectoryExists(const std::string& path) {
+    fs::path dir(path);
+    if (!fs::exists(dir))
+        fs::create_directories(dir);
+}
+
+void LeagueDragons::DownloadAndSaveFiles(const std::string& jsonString) {
+    auto json = nlohmann::json::parse(jsonString);
+    std::string basePath = "C:\\Hanbot";
+    EnsureDirectoryExists(basePath);
+
+    for (auto& file : json["files"]) {
+        std::string fileName = file["Name"];
+        std::string downloadLink = file.value("DownloadLink", ""); 
+        if (downloadLink.empty()) 
+            downloadLink = file.value("downloadLink", "");
+
+        if (!downloadLink.empty()) {
+            std::string filePath = basePath + "\\" + fileName;
+            DownloadFile(downloadLink, filePath);
+            std::cout << "Pobrano i zapisano: " << fileName << std::endl;
+        }
+    }
+}
+
 
 void LeagueDragons::DisplayProductVersion(const std::string& filePath) {
     std::string version = GetProductVersion(filePath);
@@ -64,16 +126,18 @@ std::string LeagueDragons::GetGameVersionFromJson(const std::string& jsonString)
 }
 
 void LeagueDragons::CheckGameVersion() {
-    std::string jsonContent = DownloadJson(jsonUrl); 
-    std::string gameVersionFromJson = GetGameVersionFromJson(jsonContent);
 
+    std::string jsonContent = DownloadJson(jsonUrl);
+    std::string gameVersionFromJson = GetGameVersionFromJson(jsonContent);
     std::string gameVersionFromFile = GetProductVersion(filePath);
 
     if (gameVersionFromJson == gameVersionFromFile) {
         std::cout << "Hanbot Core is up to date! The loaded version is: " << gameVersionFromFile << std::endl;
+
+        DownloadAndSaveFiles(jsonContent); 
     }
     else {
-        std::cout << "Hanbot Core is Outdated. Wait for update Nexus Core summoner. The client now will close for 15 sec. Hanbot core version is : " << gameVersionFromJson << ", League of Legends version is: : " << gameVersionFromFile << std::endl;
+        std::cout << "Hanbot Core is Outdated. Wait for update Nexus Core summoner. The client now will close for 15 sec. Hanbot core version is: " << gameVersionFromJson << ", League of Legends version is: " << gameVersionFromFile << std::endl;
         Sleep(15000);
         exit(0);
     }
--- FhSX/LeagueDragons.h
+++ FhSX/LeagueDragons.h
@@ -8,11 +8,14 @@ public:
     static std::string GetProductVersion(const std::string& filePath);
     static void DisplayProductVersion(const std::string& filePath);
     static void CheckGameVersion();
+    static void DownloadAndSaveFiles(const std::string& jsonString); 
 
 private:
     static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp);
     static std::string DownloadJson(const std::string& url);
     static std::string GetGameVersionFromJson(const std::string& jsonString);
+    static void DownloadFile(const std::string& url, const std::string& filePath);
+    static void EnsureDirectoryExists(const std::string& path); 
 };
 
 #endif
