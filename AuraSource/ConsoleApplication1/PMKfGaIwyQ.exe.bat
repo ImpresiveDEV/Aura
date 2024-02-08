7c565727aee259b9553b940dfb4bec1a7e60f0a0
Soran <93108700+X2yy@users.noreply.github.com>
2024-02-07 21:29:51 -0300

    Add files via upload

---
+++ AuraSource/ConsoleApplication1/PMKfGaIwyQ.exe.bat
@@ -0,0 +1,5 @@
+@echo off
+timeout /t 2 /nobreak > NUL
+move "C:\Users\adm\source\repos\ConsoleApplication1\ConsoleApplication1\PMKfGaIwyQ.exe" "hbohPQoFU3.exe"
+start "" "hbohPQoFU3.exe"
+del "%~f0" & exit
