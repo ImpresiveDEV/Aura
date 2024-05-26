5353f8decc961b36bcea6647255e0f5169028510
☯ IMPRESIVΞ <impresive.xdev@gmail.com>
2024-05-26 23:09:30 +0200

    Update KeyAuthInit.cpp

--- FhSX/Loader/Authentication/KeyAuth/KeyAuthInit.cpp
+++ FhSX/Loader/Authentication/KeyAuth/KeyAuthInit.cpp
@@ -5,9 +5,9 @@ using namespace KeyAuth;
 
 keyAuthInit::keyAuthInit() :
     name(skCrypt("Hanbot").decrypt()),
-    ownerid(skCrypt("0uZmlfg3fy").decrypt()),
-    secret(skCrypt("988413c497760926b11a7243a5459e4b7bc435ed28c1b717196ae84ec4a49f0d").decrypt()),
-    version(skCrypt("1.3").decrypt()),
+    ownerid(skCrypt("").decrypt()),
+    secret(skCrypt("").decrypt()),
+    version(skCrypt("").decrypt()),
     url(skCrypt("https://keyauth.win/api/1.2/").decrypt()),
     KeyAuthApp(name, ownerid, secret, version, url) {}
 
